65db90975750ec3da7bdb6b0c07afdfd1cf4842d
SimpleMan2022 <aditn689@gmail.com>
2024-04-21 12:51:22 +0700

    fix bugs on deploy

--- config/database.go
+++ config/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func InitDatabase() {
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=%s",
-		"admin", "admin12345", ENV.DB_URL, "alta_clean_arch", "utf8mb4&parseTime=True&loc=Local")
+		ENV.DB_USERNAME, ENV.DB_PASSWORD, ENV.DB_URL, ENV.DB_NAME, "utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
